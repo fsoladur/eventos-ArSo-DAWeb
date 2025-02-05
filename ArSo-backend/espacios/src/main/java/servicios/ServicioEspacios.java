694ef7b1c37873e7dbefd44f780f2700bb434024
fsoladur <fsoladur@gmail.com>
2025-02-05 18:51:08 +0100

    feat(practicas-5-febrero): diseño interfaces REST

--- ArSo-backend/espacios/src/main/java/servicios/ServicioEspacios.java
+++ ArSo-backend/espacios/src/main/java/servicios/ServicioEspacios.java
@@ -13,22 +13,63 @@ import java.util.UUID;
 
 public interface ServicioEspacios {
 
+	/**
+	 - ruta de acceso: "/espacios"
+	 - método: POST
+	 - body: parametros de alta de espacio fisico
+	 - respuesta: id del espacio fisico creado + 201 Created + location header con la URL del recurso creado
+	 */
 	String darAltaEspacioFisico(final String nombre, final String propietario, final int capacidad,
 			final String direccionPostal, final double longitud, final double latitud, final String descripcion)
 			throws RepositorioException, EntidadNoEncontrada;
 
+	/**
+	 - ruta de acceso: "/espacios/{idEspacio}/puntosinteres"
+	 - método: PUT
+	 - body: lista de puntos de interes
+	 - respuesta: 204 No Content
+	 */
 	boolean asignarPuntosInteres(final String idEspacio, Collection<PuntoInteres> puntosInteres)
 			throws RepositorioException, EntidadNoEncontrada;
 
+	/**
+	 - ruta de acceso: "/espacios/{idEspacio}"
+	 - método: PATCH
+	 - body: parametros de modificacion de espacio fisico
+	 - respuesta: 204 NO_CONTENT (la habitual) || otra opcion 200 ok más el objeto modificado
+	 */
 	EspacioFisicoDTO modificarEspacioFisico(final String idEspacio, final String nombre, final String descripcion,
 			final int capacidad) throws RepositorioException, EntidadNoEncontrada;
 
+	/**
+	 - ruta de acceso: "/espacios/{idEspacio}/estado"
+	 - método: PUT
+	 - body: enum("activo", "cerrado")
+	 - respuesta: 204 NO_CONTENT
+	 */
 	boolean darBajaEspacioFisico(final String idEspacio) throws RepositorioException, EntidadNoEncontrada;
-
 	boolean activarEspacioFisico(final String idEspacio) throws RepositorioException, EntidadNoEncontrada;
 
+	/**
+	 - ruta de acceso: "/espacios/libres?fechaInicio={fechaInicio}&fechaFin={fechaFin}&capacidadRequerida={capacidadRequerida}"
+	 - método: GET
+	 - respuesta: 200 OK, BODY:lista de espacios fisicos libres
+	 */
 	List<EspacioFisicoDTO> findEspaciosFisicosLibres(final LocalDateTime fechaInicio, final LocalDateTime fechaFin,
 			final int capacidadRequerida) throws RepositorioException, EntidadNoEncontrada;
-	
+
+	/**
+	- ruta de acceso: "/espacios?propietario={propietario}"
+	- método: GET
+	- respuesta: 200 OK, BODY:lista de espacios fisicos de un propietario
+	 */
 	List<EspacioFisicoDTO> findEspaciosFisicosDePropietario(final String propietario) throws RepositorioException, EntidadNoEncontrada;
+
+	/**
+	 - ruta de acceso: "/espacios/{idEspacio}"
+	 - método: GET
+	 - respuesta: 200 OK, BODY:espacio fisico
+	 */
+
+	EspacioFisicoDTO recuperarEspacioFisico(final String idEspacio) throws RepositorioException, EntidadNoEncontrada;
 }
--- ArSo-backend/eventos/src/main/java/servicios/ServicioEventos.java
+++ ArSo-backend/eventos/src/main/java/servicios/ServicioEventos.java
@@ -17,6 +17,14 @@ public interface ServicioEventos {
     para poder replicar los datos en este microservicio */ /* TODO esto se deberá sustituir por una llamada al microservicio de espacios físicos
      el cual nos devolverá una versión reducida y necesaria de los datos del espacio físico
      para poder replicar los datos en este microservicio */
+
+  /**
+    * Da de alta un evento en el sistema.
+   - ruta de acceso: "/eventos"
+   - método: POST
+   - parámetros: nombre, descripción, organizador, categoría, fechaInicio, fechaFin, plazas, idEspacioFisico
+    - respuesta: 201 CREATED, body: idEvento o el evento completo + cabecera location con la URI del evento creado
+   */
   String darAltaEvento(
       final String nombre,
       final String descripcion,
@@ -28,17 +36,43 @@ public interface ServicioEventos {
       final String idEspacioFisico)
       throws RepositorioException, EntidadNoEncontrada;
 
+  /**
+  - ruta de acceso: "/eventos/{idEvento}"
+    - método: PATCH
+    - parámetros: descripción, fechaInicio, fechaFin, plazas, idEspacioFisico
+    - respuesta: 204 NO CONTENT
+   */
   Evento modificarEvento(
       final String idEvento,
       final String descripcion,
       final LocalDateTime fechaInicio,
       final LocalDateTime fechaFin,
       final int plazas,
-      final EspacioFisico espacioFisico)
+      final String idEspacioFisico)
       throws RepositorioException, EntidadNoEncontrada;
 
+  /**
+  - ruta de acceso: "/eventos/{idEvento}/ocupacion"
+    - método: PUT
+    - cuerpo: --
+    - respuesta: 204 NO CONTENT
+   */
   boolean cancelarEvento(final String idEvento) throws RepositorioException, EntidadNoEncontrada;
 
+  /**
+    - ruta de acceso: "/eventos?mes={mes}"
+        - método: GET
+        - respuesta: 200 OK, body: listado eventos
+   */
   List<EventoResumenDTO> getEventosDelMes(YearMonth mes)
       throws RepositorioException, EntidadNoEncontrada;
+
+  /**
+    - ruta de acceso: "/eventos/{idEvento}"
+        - método: GET
+        - respuesta: 200 OK, body: evento
+   */
+
+  // es necesario este método faltaba antes
+    Evento recuperarEvento(final String idEvento) throws RepositorioException, EntidadNoEncontrada;
 }
