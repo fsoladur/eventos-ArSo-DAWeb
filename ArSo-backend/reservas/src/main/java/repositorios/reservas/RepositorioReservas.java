3cfeaf0e6c0814c0980160783b80357b20842a9c
fsoladur <fsoladur@gmail.com>
2025-03-13 13:27:55 +0100

    feat(servicio-repositorio-reservas): se adjunta el servicio de reservas que hace uso de los distintos repositorios definidos

---
+++ ArSo-backend/reservas/src/main/java/repositorios/reservas/RepositorioReservas.java
@@ -0,0 +1,15 @@
+package repositorios.reservas;
+
+import dominio.Reserva;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+@NoRepositoryBean
+public interface RepositorioReservas extends CrudRepository<Reserva, UUID>, PagingAndSortingRepository<Reserva, UUID> {
+    Page<Reserva> findAllByEventoId(UUID eventoId, Pageable pageable);
+}
