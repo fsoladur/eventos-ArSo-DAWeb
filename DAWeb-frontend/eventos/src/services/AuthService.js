265a67ce2220e866238d279e30392cc8b8904cdd
fsoladur <fsoladur@gmail.com>
2025-04-23 03:00:03 +0200

    fix(login-mejorado): se ha mejorado la lógica del login

---
+++ DAWeb-frontend/eventos/src/services/AuthService.js
@@ -0,0 +1,18 @@
+export const login = async dto => {
+  const response = await fetch('http://localhost:8090/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      username: dto.username,
+      password: dto.password
+    })
+  });
+
+  if (!response.ok) {
+    throw new Error('Usuario o contraseña incorrectos');
+  }
+  const data = await response.json();
+  return data;
+};
